c0215acb6b9e7da030d2df94c2e1087bc2a3ec3b
RobertoRamirez0 <ramirez.robertomu22@gmail.com>
2019-08-28 19:48:24 -0400

    created file structure, set up and tested heading function and checked import and exports for the function; all in working order

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
+import Heading from './Heading';
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Heading />
+      <Content />
+      <Footer />
     </div>
   );
 }
---
+++ src/Heading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// This function sets up groundwork for the heading element of the app.
+
+
+function Heading(){
+  return (
+  <div className='heading'>
+    <h1>NASA Photo of the Day</h1>
+    <p>A React Web App by Roberto Ramirez</p>
+  </div>
+  )
+};
+
+export default Heading;
